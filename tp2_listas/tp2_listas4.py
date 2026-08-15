17a871c6c9f6c38fbd1c46ccb8b31a1857c26e22
Valentin Ian Gonzalez <vigonzalez500@gmail.com>
2026-08-15 00:52:51 -0300

    Creo tp2_listas4.py

---
+++ tp2_listas/tp2_listas4.py
@@ -0,0 +1,31 @@
+'''
+Actividad 4
+Crea una lista sin elementos repetidos a partir de una lista predefinida y muestra el resultado.
+'''
+# Lista predefinida
+data = [1, 3, 5, 3, 7, 1, 9, 5, 3]
+
+# -----------------------------
+# Lista sin elementos repetidos
+# -----------------------------
+unique_data = []
+for item in data:
+    if item not in unique_data:
+        unique_data.append(item)
+
+# --------------------------------------------------
+# Bucles para mostrar la comparación de ambas listas
+#---------------------------------------------------
+print('>> Comparación ambas listas')
+
+# Bucle para mostrar la lista original
+print('Lista de datos original:')
+for i in range(len(data)):
+    if i != len(data) - 1:
+        print(f'{data[i]}, ', end='')
+    else:
+        print(data[i], end='\n')
+
+# Bucle para mostrar la lista sin los elementos repetidos
+print('Lista de datos sin elementos repetidos:')
+print(*unique_data, sep=', ')
