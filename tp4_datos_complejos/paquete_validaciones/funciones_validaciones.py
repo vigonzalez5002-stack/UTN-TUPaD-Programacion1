48facfb20eb15bd602acf8e9b3faa3c5faf7e129
Valentin Ian Gonzalez <vigonzalez500@gmail.com>
2026-08-30 01:04:44 -0300

    Modificaciones a la función ingresar_texto

--- tp4_datos_complejos/paquete_validaciones/funciones_validaciones.py
+++ tp4_datos_complejos/paquete_validaciones/funciones_validaciones.py
@@ -3,5 +3,55 @@
 # de datos ingresados por el usuario.
 # ---------------------------------------------------------
 
-def ingresar_telefono():
-    pass
+def ingresar_texto(prompt = '', text_valid = [], not_text_valid = False):
+    '''
+    Esta función es un bucle de validación que controla el ingreso
+    de textos. Este bucle se terminará y retornará el texto si es 
+    un string de únicamente letras.
+    
+    El parámetro prompt permite darle indicaciones de que ingresar al
+    usuario.
+
+    El parámetro text_valid permite establecer una lista de textos válidos.
+
+    El parámetro not_text_valid invertir la lógica del parámetro text_valid,
+    permitiendo admitir como válido el ingreso de textos que no se encuentran
+    en la lista.
+    '''
+
+    while True:
+        text = input(prompt).strip()
+
+        if not text.isalpha(): # Si el texto tiene carácteres inválido
+            print('Error: Solo se admiten letras.')
+            
+        elif text_valid != []: #Si el text_valid no es vacío 
+            # Cambio de condición por el parámetro not_text_valid
+            text_valid_condition = text not in text_valid
+            text_valid_error = 'Error: No se encuentra entre las opciones.'
+            if not_text_valid:
+                text_valid_condition = text in text_valid
+                text_valid_error = f'Error: {text} ya está agregado'
+
+            if text_valid_condition: # Condición del text_valid
+                print(text_valid_error)
+            else:
+                return text
+            
+        else:
+            return text
+            
+
+def ingresar_numero():
+    '''
+    Esta función es un bucle de validación que controla el ingreso de un
+    número de teléfono. Este bucle se terminará y retornará el número de
+    teléfono si se ingresa un número de entero válido.
+    '''
+
+    while True:
+        phone_number = input('Ingrese un número de teléfono: ').strip()
+        if phone_number.isdigit() and len(phone_number) <= 15:
+            return int(phone_number)
+        else:
+            print('Error: Número de teléfono inválido, ingrese un número válido que no exceda los 15 dígitos.')
--- tp4_datos_complejos/tp4_datos_complejos4.py
+++ tp4_datos_complejos/tp4_datos_complejos4.py
@@ -4,5 +4,33 @@ Programa que almacena números telefónicos a un registro y busca el el número
 dentro del registro.
 '''
 
-# Se hará uso de un paquete para la validación de datos y así evitar repetir código.
+# Se hará uso de un paquete para la validación de datos y así evitar repetir código
+from paquete_validaciones.funciones_validaciones import ingresar_numero, ingresar_texto
 
+# ---------------------------------------------------------------------------------
+# El siguiente código almacenta a un diciconario 5 números de contacto.
+# Los pares clave-valor corresponden al nombre del contacto y el número de teléfono
+# de los contactos respectivamente.
+# ---------------------------------------------------------------------------------
+
+contact_dictionary = {}
+for i in range(1, 6):
+    print(f'\nIngrese el contacto Nº{i}')
+    name = ingresar_texto('Ingresa el nombre del contacto: ', list(contact_dictionary.keys()), not_text_valid = True)
+    phone_number = ingresar_numero()
+    contact_dictionary[name] = phone_number 
+print()
+
+# --------------------------------------------------------------------------------
+# El siguiente código busca el número de contacto dentro del diccionario a partir
+# del nombre de contacto. Para eseto se usa un bucle de validación para asegurarse
+# de que el contacto a ingresar se encuentre registrado en el diccionario.
+# --------------------------------------------------------------------------------
+
+contact_list = list(contact_dictionary.keys()) # Lista de los nombres de los contactos
+
+# Esto valida el nombre del contacto
+contact = ingresar_texto('Ingresa el nombre del contacto a buscar: ', contact_list)
+
+# Esto imprime el número de contacto correspondiente
+print(f'\nEl número de teléfono de {contact} es {contact_dictionary.get(contact)}')
