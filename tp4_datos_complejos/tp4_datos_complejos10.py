cf3190ce2184e9d9ab8e6268273d7b9e3ec65251
Valentin Ian Gonzalez <vigonzalez500@gmail.com>
2026-08-31 14:25:14 -0300

    - Crea el archivo tp4_datos_complejos10.py

---
+++ tp4_datos_complejos/tp4_datos_complejos10.py
@@ -0,0 +1,28 @@
+'''
+Actividad 10
+El programa invierte los pares clave-valor de un diccionario que mapea los nombres de los países
+con sus capitales. De esta forma, el diccionario invertido será de pares clave-valor que mapea
+los nombres de las capitales con sus países.
+'''
+from paquete_texto.funciones_texto import imprimir_diccionario
+
+# Diccionario original
+original_dictionary = {
+    'Argentina': 'Buenos Aires',
+    'Chie': 'Santiago',
+}
+
+# --------------------------------------------------------------------------
+# Este código crea un nuevo diccionario con los pares clave-valor invertidos
+# y los imprime en la terminal.
+# --------------------------------------------------------------------------
+
+reverse_dictionary = {}
+
+# Esto construye el nuevo diccionario con los pares invertidos
+for key, value in original_dictionary.items():
+    reverse_dictionary[value] = key
+
+# Esto muestra en la terminal ambos diccionarios
+imprimir_diccionario(original_dictionary, 'Diccionario original:')
+imprimir_diccionario(reverse_dictionary, 'Diccionario invertido:')
